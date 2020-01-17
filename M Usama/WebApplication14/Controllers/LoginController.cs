28fab9492c091e6de91d0a92b7d5f5daad683d6d
Mrusama719 <mrrusama01@gmail.com>
2020-01-17 17:21:46 +0500

    first commit

---
+++ M Usama/WebApplication14/Controllers/LoginController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication14.Models;
+
+namespace WebApplication14.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(User Us)
+        {
+            
+            return View();
+        }
+    }
+}
